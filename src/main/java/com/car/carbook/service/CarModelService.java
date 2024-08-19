cd2cab3bb4d165b9b0f2d5e90f1bc27719c156d2
shakodavitashvili <s.davitashvili@optimogroup.io>
2024-08-19 22:49:55 +0400

    remove import

--- src/main/java/com/car/carbook/service/CarModelService.java
+++ src/main/java/com/car/carbook/service/CarModelService.java
@@ -8,7 +8,6 @@ import com.car.carbook.repository.CarModelRepository;
 import com.car.carbook.service.Mapper.CarModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
